f1f40d1a173c7153ee4cd377986352c58ae24cba
nokekip <kipnoke@gmail.com>
2024-03-10 18:36:41 +0300

    fix pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,26 +3,26 @@
 
 
 def pascal_triangle(n):
-    """Returns a list of lists of integers 
+    """Returns a list of lists of integers
     representing the Pascal’s triangle of n:
     """
 
     if n <= 0:
         return []
 
-    
     """ initialize an empty resulting array """
     pascal_list = [[] for idx in range(n)]
 
     for li in range(n):
         for col in range(li+1):
-            if(col < li):
-                if(col == 0):
+            if (col < li):
+                if (col == 0):
                     """ the first column is always set to 1 """
                     pascal_list[li].append(1)
                 else:
-                    pascal_list[li].append(pascal_list[li-1][col] + pascal_list[li-1][col-1])
-            elif(col == li):
+                    pascal_list[li].append(
+                        pascal_list[li-1][col] + pascal_list[li-1][col-1])
+            elif (col == li):
                 """ the diagonal is always set to 1 """
                 pascal_list[li].append(1)
 
